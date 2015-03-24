f84c29d81fa88207477dfda7800db529bc560078
Tomek Gryszkiewicz <grych@tg.pl>
2015-03-24 10:11:44 +0100

    item add

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,8 @@ class Item < ActiveRecord::Base
   belongs_to :directory
 
   validates :password_crypted, presence: true
+  # validates :password, presence: true
+
   validate  :have_groups
 
   # default_scope { order('username ASC') }
@@ -59,6 +61,52 @@ class Item < ActiveRecord::Base
     end
   end
 
+  # Associate +other+ object with the item. 
+  #
+  #   i = Item.new(username:'u', password:'p'); g=Group.first; u=User.first; u.password='password0'
+  #   i.add g
+  #   #=> #<Group:0x007fbbef4187e8>
+  def add(other, **options)
+    authenticator = options[:authorization_user] || @authorization_user
+    cipher = OpenSSL::Cipher::AES256.new(:CBC)
+    case other
+    when Group
+      if self.new_record?
+        # user not needed
+        new_item_key, new_item_iv = cipher.random_key, cipher.random_iv
+        key_crypted, iv_crypted = other.public_key.public_encrypt(new_item_key), other.public_key.public_encrypt(new_item_iv)
+        self.password_crypted = encrypt(self.password, new_item_key, new_item_iv)
+        self.groups << other
+        meta = self.meta_keys.find {|x| x.group == other}
+        raise "Couldn't find the corresponding meta" unless meta
+        meta.key_crypted, meta.iv_crypted = key_crypted, iv_crypted
+        other
+      else
+        # have to decrypt
+        raise Tarkin::NotAuthorized, "This operation must be autorized by valid user" unless authenticator
+        authenticator_meta, authenticator_group = meta_and_group_for_user authenticator
+        authenticator_group_private_key = authenticator_group.private_key(authorization_user: authenticator)
+        item_key, item_iv = authenticator_group_private_key.private_decrypt(authenticator_meta.key_crypted),
+                            authenticator_group_private_key.private_decrypt(authenticator_meta.iv_crypted)
+        key_crypted, iv_crypted = other.public_key.public_encrypt(item_key), other.public_key.public_encrypt(item_iv)
+        other.meta_keys.new item: self, key_crypted: key_crypted, iv_crypted: iv_crypted
+        other
+        # self.items(true)
+        # other.groups(true)
+      end
+    end
+  end
+
+  # Operator similar to #add method. Requires #authorize before:
+  #
+  #   item.authorize user
+  #   item << Group.first
+  def <<(other)
+    raise Tarkin::NotAuthorized, "This operation must be autorized by valid user" unless @authorization_user
+    add(other, authorization_user: @authorization_user)
+  end
+
+
   # Shorter view - to prevent rails console to show all the encrypted data
   def inspect
     "#<Item> '#{self.username}'  [id: #{self.id}, directory_id: #{self.directory_id}]"
@@ -74,9 +122,17 @@ class Item < ActiveRecord::Base
   # as the intersections between two group arrays
   def meta_and_group_for_user(user)
     groups = user.groups & self.groups
-    raise Tarkin::ItemNotAccessibleException, "Group association not found for item #{self.id} and user #{user.id}" if groups.nil? || groups.empty? || groups.length != 1
+    raise Tarkin::ItemNotAccessibleException, "Group association not found for item #{self.id} and user #{user.id}" if groups.nil? || groups.empty? 
     group = groups.first
     meta = self.meta_keys.find_by(group: group)
+    raise Tarkin::ItemNotAccessibleException, "Item #{self.id} does not belong to #{group.name}" unless meta
+    [meta, group]
+  end
+  def meta_and_group_for_user_and_item(user, item)
+    groups = user.groups & item.groups
+    raise Tarkin::ItemNotAccessibleException, "Group association not found for item #{item.id} and user #{user.id}" if groups.nil? || groups.empty? 
+    group = groups.first
+    meta = item.meta_keys.find_by(group: group)
     raise Tarkin::ItemNotAccessibleException, "Item #{self.id} does not belong to #{group.name}" unless meta
     [meta, group]
   end
