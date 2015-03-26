c86a29518a555319de73cd8dcbcbd92f1e9ccf2d
Tomek Gryszkiewicz <grych@tg.pl>
2015-03-26 12:19:04 +0100

    assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,17 +9,18 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( sessions.js directories.js )
-Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+
+# Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
 # Rails.application.config.assets.precompile << Proc.new do |path|
-#   if path =~ /\.(css|js)\z/
+#   if path =~ /\.(css|js|svg|eot|woff|ttf)\z/
 #     full_path = Rails.application.assets.resolve(path).to_path
 #     app_assets_path = Rails.root.join('app', 'assets').to_path
 #     if full_path.starts_with? app_assets_path
-#       puts "including asset: " + full_path
+#       # puts "including asset: " + full_path
 #       true
 #     else
-#       puts "excluding asset: " + full_path
+#       # puts "excluding asset: " + full_path
 #       false
 #     end
 #   else
