e83104ed65168f8fd9b4e56ace56c12b50bd4a12
Tomek Gryszkiewicz <grych@tg.pl>
2015-04-02 16:20:03 +0200

    favorites list now refreshed via ajax

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -47,7 +47,7 @@ module SessionsHelper
   def signed_in_user
     unless signed_in?
       store_location
-      redirect_to new_session_path, notice: "Please sign in."
+      redirect_to new_session_path #, notice: "Please sign in."
     end
   end
 
