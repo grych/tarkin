a2d7b96b169e116a9ed52e0c7f28992674a2c43a
Tomek Gryszkiewicz <grych@tg.pl>
2015-04-23 17:00:01 +0200

    user edition

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -38,6 +38,21 @@ class Admin::UsersController < Admin::AdminController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    if @user == current_user
+      flash.now[:error] = "Suicide denied"
+      render 'edit'
+    else
+      if @user.destroy
+        redirect_to users_path #, notice: 'user deleted'
+      else
+        flash.now[:error] = "User can't be deleted, because #{@user.errors.full_messages.join(' and ')}."
+        render 'edit'
+      end
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :admin, :token_expires_in, :password)
