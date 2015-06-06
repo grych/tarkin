3133995ea9c66aad14bf4939f403257e338ae31c
Tomek Gryszkiewicz <grych@tg.pl>
2015-06-06 15:20:51 +0200

    secret

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,18 +14,18 @@ Rails.application.config.assets.precompile += %w( jquery-tablesorter/dropbox-asc
 Rails.application.config.assets.precompile += %w( admin/users.js )
 # Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
-# Rails.application.config.assets.precompile << Proc.new do |path|
-#   if path =~ /\.(css|js|svg|eot|woff|ttf)\z/
-#     full_path = Rails.application.assets.resolve(path).to_path
-#     app_assets_path = Rails.root.join('app', 'assets').to_path
-#     if full_path.starts_with? app_assets_path
-#       # puts "including asset: " + full_path
-#       true
-#     else
-#       # puts "excluding asset: " + full_path
-#       false
-#     end
-#   else
-#     false
-#   end
-# end
+Rails.application.config.assets.precompile << Proc.new do |path|
+  if path =~ /\.(css|js|svg|eot|woff|ttf)\z/
+    full_path = Rails.application.assets.resolve(path).to_path
+    app_assets_path = Rails.root.join('app', 'assets').to_path
+    if full_path.starts_with? app_assets_path
+      # puts "including asset: " + full_path
+      true
+    else
+      # puts "excluding asset: " + full_path
+      false
+    end
+  else
+    false
+  end
+end
